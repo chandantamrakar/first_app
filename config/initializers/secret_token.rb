d201ea5731ecc0d6e8cba32fecb88d817fa65f71
nepasoft <nepasoft@ubuntu.(none)>
2013-06-28 00:02:41 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Firstapp::Application.config.secret_key_base = 'b9f41c17765e7c7c37c270baaf7aac852d1a355889405bd26e9d27a9ec49c5594649c37b9803690adf89239c2c886645ada83b9b231b7a611aa31ebc38dc23f7'
